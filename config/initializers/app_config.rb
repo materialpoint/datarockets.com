a1520339f00e37556aa197326926227c18829cda
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2014-08-25 19:29:24 +0300

    Try to fix heroku deployment issue with app_config.yml existence

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,4 +1,10 @@
 config_path = Rails.root.join('config/app_config.yml')
+
+# heroku loads environment during deployment but config/app_config.yml doesn't exitst.
+unless File.exist?(config_path)
+  config_path = Rails.root.join('config/examples/app_config.yml')
+end
+
 yaml_config = ERB.new(File.read(config_path)).result
 config = YAML.load(yaml_config)
 AppConfig.setup!(config)
