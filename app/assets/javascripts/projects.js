cc4ddba1e898c30fc211e33313d62cbf63c241f9
Maxim Abramchuk <maximabramchuck@gmail.com>
2014-09-02 14:34:05 +0300

    Add exception processing for the ajax request

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -5,6 +5,7 @@ jQuery(function() {
       $('#project_description').after('<input id="project_project_image" name="project[project_image_ids][]" type="hidden" value="'+data.result.id+'">');
       $('#file_upload').after('<br><img src="'+data.result.url+'"><b>'+'![place alt text here]('+data.result.url+')</b>')
     }
+    fail: function(e, data) { alert 'Upload failed' }
   });
 });
 
