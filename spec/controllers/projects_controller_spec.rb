05204f2d6567af35c3250d9eceb5d168eb7b4165
Maxim Abramchuk <maximabramchuck@gmail.com>
2014-09-05 17:48:39 +0300

    Merge test for the controllers into one file

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,3 +1,7 @@
 RSpec.describe Admin::ProjectsController do
   it_behaves_like 'an admin controller'
 end
+
+RSpec.describe ProjectsController do
+  it_behaves_like 'a controller'
+end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,3 +1,7 @@
 RSpec.describe Admin::QuestionsController do
   it_behaves_like 'an admin controller'
 end
+
+RSpec.describe QuestionsController do
+  it_behaves_like 'a controller'
+end
