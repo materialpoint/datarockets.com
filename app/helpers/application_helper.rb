44daeffb8ba9c5c2a1637b2e2a0590126e9ae11e
Maxim Abramchuk <maximabramchuck@gmail.com>
2014-09-07 19:36:10 +0300

    Add syntax highlighling using github theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,27 @@
 module ApplicationHelper
+  class HTML < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Rouge.highlight(code, language || 'text', 'html')
+    end
+  end
+
   def markdown(text)
-    options = {
+    render_options = {
       filter_html: true,
       hard_wrap: true,
-      link_attributes: {},
-      space_after_headers: true,
-      fenced_code_blocks: true,
-      tables: true
+      link_attributes: { rel: 'nofollow' }
     }
 
     extensions = {
       autolink: true,
-      superscript: true,
-      disable_indented_code_blocks: true,
-      tables: true
+      fenced_code_blocks: true,
+      lax_spacing: true,
+      no_intra_emphasis: true,
+      strikethrough: true,
+      superscript: true
     }
 
-    renderer = Redcarpet::Render::HTML.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
-    markdown.render(text).html_safe
+    renderer = HTML.new(render_options)
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
   end
 end
