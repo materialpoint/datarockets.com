14467f7edff13ce47da9303e1c0c139cdc329ef7
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2014-09-07 13:02:32 +0300

    Don't use vertical align

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
   def markdown(text)
     options = {
-      filter_html:     true,
-      hard_wrap:       true,
-      link_attributes: { rel: 'nofollow', target: "_blank" },
+      filter_html: true,
+      hard_wrap: true,
+      link_attributes: {},
       space_after_headers: true,
       fenced_code_blocks: true,
       tables: true
     }
 
     extensions = {
-      autolink:           true,
-      superscript:        true,
+      autolink: true,
+      superscript: true,
       disable_indented_code_blocks: true,
       tables: true
     }
