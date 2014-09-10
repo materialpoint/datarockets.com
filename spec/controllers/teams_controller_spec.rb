d1c2648432f7fcc45539a8cdf231cdc1bf5375ed
Maxim Abramchuk <maximabramchuck@gmail.com>
2014-09-10 14:42:49 +0300

    Fixed fail

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 describe TeamsController do
   describe '#show' do
-    it 'return all members' do
-      member = Fabricate(:user_member)
-      Fabricate(:user_admin) #need for checking filter
+    it 'return all users' do
+      member = Fabricate(:user)
 
       get :show
 
-      expect(assigns(:members)).to eq([member])
+      expect(assigns(:team)).to eq([member])
     end
   end
 end
