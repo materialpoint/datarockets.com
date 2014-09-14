2c68b038dd42d2fc6081938e2ea0208936f5ca52
Maxim Abramchuk <maximabramchuck@gmail.com>
2014-09-14 21:51:05 +0300

    Improve specs

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -4,10 +4,11 @@ describe TeamsController do
   describe '#show' do
     it 'return all users' do
       member = Fabricate(:user)
+      admin = Fabricate(:user_admin)
 
       get :show
 
-      expect(assigns(:team)).to eq([member])
+      expect(assigns(:team)).to eq([member, admin])
     end
   end
 end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -2,34 +2,48 @@ require 'rails_helper'
 
 describe UserPolicy do
   subject { UserPolicy }
-  let(:target_user) { Fabricate.build(:user) }
 
   permissions :manage? do
+    let(:member) { Fabricate(:user) }
+    let(:admin) { Fabricate(:user_admin) }
+
     context 'for guest' do
       let(:current_user) { nil }
 
-      it 'denies access' do
-        expect(subject).to_not permit(current_user, target_user)
+      it 'denies access to change member' do
+        expect(subject).to_not permit(current_user, member)
+      end
+
+      it 'denies access to change admin' do
+        expect(subject).to_not permit(current_user, admin)
       end
     end
 
     context 'for member' do
       let(:current_user) { Fabricate.build(:user) }
 
-      it 'denies access' do
-        expect(subject).to_not permit(current_user, target_user)
+      it 'denies access to change member' do
+        expect(subject).to_not permit(current_user, member)
+      end
+
+      it 'denies access to change admin' do
+        expect(subject).to_not permit(current_user, admin)
       end
     end
 
     context 'for admin' do
       let(:current_user) { Fabricate.build(:user_admin) }
 
-      it 'permit access' do
-        expect(subject).to permit(current_user, target_user)
+      it 'permit access to change member' do
+        expect(subject).to permit(current_user, member)
       end
 
       it 'permit access to change himself' do
         expect(subject).to permit(current_user, current_user)
+      end
+
+      it 'permit access to change admin' do
+        expect(subject).to permit(current_user, admin)
       end
     end
   end
