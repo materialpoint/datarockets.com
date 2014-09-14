16580b0c8774f3f13585b541852cccd2016cd00a
Maxim Abramchuk <maximabramchuck@gmail.com>
2014-09-14 07:15:01 +0300

    Fix user names in policy specs

--- spec/policies/page_policy_spec.rb
+++ spec/policies/page_policy_spec.rb
@@ -5,49 +5,49 @@ describe PagePolicy do
   let(:page) { Fabricate.build(:page) }
 
   context 'for guest' do
-    let(:user) { nil }
+    let(:guest) { nil }
 
     permissions :show? do
       it 'permit access' do
-        expect(subject).to permit(user, page)
+        expect(subject).to permit(guest, page)
       end
     end
 
     permissions :manage? do
       it 'denies access' do
-        expect(subject).not_to permit(user, page)
+        expect(subject).not_to permit(guest, page)
       end
     end
   end
 
   context 'for member' do
-    let(:user) { Fabricate.build(:user, role: :member) }
+    let(:member) { Fabricate.build(:user, role: :member) }
 
     permissions :show? do
       it 'permit access' do
-        expect(subject).to permit(user, page)
+        expect(subject).to permit(member, page)
       end
     end
 
     permissions :manage? do
       it 'denies access' do
-        expect(subject).not_to permit(user, page)
+        expect(subject).not_to permit(member, page)
       end
     end
   end
 
   context 'for admin' do
-    let(:user) { Fabricate.build(:user, role: :admin) }
+    let(:admin) { Fabricate.build(:user, role: :admin) }
 
     permissions :show? do
       it 'permit access' do
-        expect(subject).to permit(user, page)
+        expect(subject).to permit(admin, page)
       end
     end
 
     permissions :manage? do
       it 'permit access' do
-        expect(subject).to permit(user, page)
+        expect(subject).to permit(admin, page)
       end
     end
   end
