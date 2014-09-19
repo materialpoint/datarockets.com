0dd5f754482b4feed214a1a48a8c9207c2e7515c
Maxim Abramchuk <maximabramchuck@gmail.com>
2014-09-19 17:35:31 +0300

    Some improvements

--- app/helpers/admin_projects_helper.rb
+++ app/helpers/admin_projects_helper.rb
@@ -1,7 +1,7 @@
 module AdminProjectsHelper
-  def render_preview_image
-    if @project.preview_image && @project.preview_image.persisted?
-      image_tag(@project.preview_image.image.url(:thumb))
+  def render_project_preview_image(project)
+    if project.preview_image && project.preview_image.persisted?
+      image_tag(project.preview_image.image.url(:thumb))
     end
   end
 end
