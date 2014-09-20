07445bb8a99e8de18a4a10a0da337661443a6d99
Maxim Abramchuk <maximabramchuck@gmail.com>
2014-09-20 19:32:21 +0300

    Add tests for checking preview image building

--- spec/controllers/admin/projects_controller_spec.rb
+++ spec/controllers/admin/projects_controller_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 describe Admin::ProjectsController do
   it_behaves_like 'admin resource controller'
+
+  for_user :admin do
+    describe 'GET #new' do
+      it 'builds a preview image' do
+        get :new
+
+        expect(assigns(:project).preview_image).to be_a_new(PreviewImage)
+      end
+    end
+
+    describe 'GET #edit' do
+      let(:project) { Fabricate(:project) }
+
+      it 'builds a preview image' do
+        get :edit, id: project.id
+
+        expect(assigns(:project).preview_image).to be_a_new(PreviewImage)
+      end
+    end
+  end
 end
