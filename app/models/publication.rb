1838553354477350dd034b32a879fc7864cd7482
Maxim Abramchuk <maximabramchuck@gmail.com>
2014-09-25 15:06:02 +0300

    Use symbols instead of strings

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,4 +1,4 @@
 class Publication < ActiveRecord::Base
   belongs_to :post
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :author, class_name: :User, foreign_key: :user_id
 end
--- app/models/working.rb
+++ app/models/working.rb
@@ -1,4 +1,4 @@
 class Working < ActiveRecord::Base
   belongs_to :project
-  belongs_to :developer, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :developer, class_name: :User, foreign_key: :user_id
 end
