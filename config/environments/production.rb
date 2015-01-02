aaeeb29661d5b684c3664eb0622df0f24126c7ff
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2015-01-02 21:43:46 +0300

    Add digest to asset files in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
-
+  config.assets.digest = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
