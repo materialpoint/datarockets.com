65f005b750f125667c7703429934c3c066c4388a
nataliagorevaya <nataliagorevaya@gmail.com>
2015-02-05 12:47:15 +0300

    Add app_config.yml to linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :format, :pretty
 set :log_level, :info
 set :pty, true
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/app_config.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
